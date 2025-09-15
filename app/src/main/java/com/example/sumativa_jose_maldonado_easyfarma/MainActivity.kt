db5414fa7e9948710249a0d17ec9ede5ce1e2e42
jama2210 <josemaldo2214@gmail.com>
2025-09-15 09:02:40 -0300

    levantando el proyecto

---
+++ app/src/main/java/com/example/sumativa_jose_maldonado_easyfarma/MainActivity.kt
@@ -0,0 +1,47 @@
+package com.example.sumativa_jose_maldonado_easyfarma
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.sumativa_jose_maldonado_easyfarma.ui.theme.Sumativa_jose_maldonado_easyFarmaTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            Sumativa_jose_maldonado_easyFarmaTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    Sumativa_jose_maldonado_easyFarmaTheme {
+        Greeting("Android")
+    }
+}
